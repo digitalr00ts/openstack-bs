049e23c1f76e011db8e6f59c1d076ceb03de1852
Carlos Meza <carlos@digitalr00ts.com>
2016-08-16 19:53:35 -0700

    add openstack repo and packages

--- base.sh
+++ base.sh
@@ -17,3 +17,12 @@ $IP_CONTROLLER		controller
 # compute1
 $IP_COMPUTE1		compute1
 EOT"
+
+#Packages
+#http://docs.openstack.org/mitaka/install-guide-ubuntu/environment-packages.html
+sudo apt-get -o Dpkg::Options::="--force-confnew" --force-yes --assume-yes install software-properties-common
+sudo add-apt-repository --yes cloud-archive:mitaka
+echo 'apt-get update' ; sudo apt-get -qqy update && \
+sudo apt-get -o Dpkg::Options::="--force-confnew" --force-yes --assume-yes dist-upgrade
+sudo apt-get -o Dpkg::Options::="--force-confnew" --force-yes --assume-yes install python-openstackclient
+sudo apt-get -o Dpkg::Options::="--force-confnew" --force-yes --assume-yes --no-install-recommends install python-pymysql
