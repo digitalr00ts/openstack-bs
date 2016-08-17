df2b8d6a2615f81be4d07a759fab5dedfa6aaaee
Carlos Meza <carlos@digitalr00ts.com>
2016-08-16 17:28:48 -0700

    add run.sh

---
+++ run.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+if [ "$1" == 'compute' ] || [ "$1" == 'controller' ]; then
+  NODE_TYPE="$1"
+fi
+
+if [ -z $NODE_TYPE ]; then
+  echo "Runs scripts based on node type specified."
+  echo "Usage: $(basename "$0") <compute|controller>"
+  exit 1
+fi
+
+which -s curl
+if [ $? ]; then
+  echo "ERROR: curl command not found."
+  exit 1
+fi
+
+[ ! -f ./config.ini ] && curl --progress-bar -LO https://github.com/digitalr00ts/openstack-bs/raw/master/config.ini
+. config.ini
+
+
+if [ "$NODE_TYPE" == 'controller' ]; then
+  mkdir -p controller
+
+  [ ! -f ./controller/environment.sh ] && curl --progress-bar -LO https://github.com/digitalr00ts/openstack-bs/raw/controller/environment.sh && chmod +x controller/environment.sh
+  ./controller/environment.sh
+
+  [ ! -f ./controller/keystone.sh ] && curl --progress-bar -LO https://github.com/digitalr00ts/openstack-bs/raw/controller/keystone.sh && chmod +x controller/keystone.sh
+  ./controller/keystone.sh
+
+  [ ! -f ./controller/glance.sh ] && curl --progress-bar -LO https://github.com/digitalr00ts/openstack-bs/raw/controller/glance.sh && chmod +x controller/glance.sh
+  ./controller/glance.sh
+
+  [ ! -f ./controller/nova.sh ] && curl --progress-bar -LO https://github.com/digitalr00ts/openstack-bs/raw/controller/nova.sh && chmod +x controller/nova.sh
+  ./controller/nova.sh
+
+  [ ! -f ./controller/neutron.sh ] && curl --progress-bar -LO https://github.com/digitalr00ts/openstack-bs/raw/controller/neutron.sh && chmod +x controller/neutron.sh
+  ./controller/neutron.sh
+
+elif [ "$NODE_TYPE" == 'compute' ]; then
+  mkdir -p compute
+
+  [ ! -f ./compute/nova.sh ] && curl --progress-bar -LO https://github.com/digitalr00ts/openstack-bs/raw/compute/nova.sh && chmod +x compute/nova.sh
+  ./compute/nova.sh
+
+  [ ! -f ./compute/neutron.sh ] && curl --progress-bar -LO https://github.com/digitalr00ts/openstack-bs/raw/compute/neutron.sh && chmod +x compute/neutron.sh
+  ./compute/neutron.sh
+
+fi
